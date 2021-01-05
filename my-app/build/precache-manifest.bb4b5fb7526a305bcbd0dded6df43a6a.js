f4807166a4f5bfbd706d14b2fe1078b338ab1b0d
Jocelyn Sun <jocelyn.z.sun@berkeley.edu>
2021-01-04 17:08:45 -0800

    ghpage deploy

---
+++ my-app/build/precache-manifest.bb4b5fb7526a305bcbd0dded6df43a6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce49d5de486419b2b77e6d8f1a332e17",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "277d253439e52f5ee966",
+    "url": "/simple-todo-app/static/css/main.26623ec4.chunk.css"
+  },
+  {
+    "revision": "db8ad9d73551883f0ca6",
+    "url": "/simple-todo-app/static/js/2.5e458010.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/simple-todo-app/static/js/2.5e458010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277d253439e52f5ee966",
+    "url": "/simple-todo-app/static/js/main.87943347.chunk.js"
+  },
+  {
+    "revision": "01a29bd6c165c3ad1016",
+    "url": "/simple-todo-app/static/js/runtime-main.80cc3432.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simple-todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
